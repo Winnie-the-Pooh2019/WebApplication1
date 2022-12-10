0e91f2348562beae842789ddd11d188a0cddc930
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2022-12-10 22:08:26 +0700

    db gen script added

--- res/sql/start.sql
+++ res/sql/start.sql
@@ -9,10 +9,18 @@ drop table if exists purchases cascade;
 drop table if exists purchase_item cascade;
 
 begin;
-\i '/home/ivan/RiderProjects/WebApplication1/res/sql/create.sql';
+\i '/home/ivan/RiderProjects/samples/WebApplication1/res/sql/create.sql';
 
-\copy categories from '';
+\copy categories from '/home/ivan/RiderProjects/samples/WebApplication1/res/data/categories.csv' with csv header delimiter ';';
+\copy books from '/home/ivan/RiderProjects/samples/WebApplication1/res/data/books.csv' with csv header delimiter ';';
+\copy clients from '/home/ivan/RiderProjects/samples/WebApplication1/res/data/clients.csv' with csv header delimiter ';';
+\copy deliveries from '/home/ivan/RiderProjects/samples/WebApplication1/res/data/deliveries.csv' with csv header delimiter ';';
+\copy price_change from '/home/ivan/RiderProjects/samples/WebApplication1/res/data/price_change.csv' with csv header delimiter ';';
+\copy publisher from '/home/ivan/RiderProjects/samples/WebApplication1/res/data/publisher.csv' with csv header delimiter ';';
+\copy purchase_item from '/home/ivan/RiderProjects/samples/WebApplication1/res/data/purchase_item.csv' with csv header delimiter ';';
+\copy purchases from '/home/ivan/RiderProjects/samples/WebApplication1/res/data/purchases.csv' with csv header delimiter ';';
+\copy store from '/home/ivan/RiderProjects/samples/WebApplication1/res/data/store.csv' with csv header delimiter ';';
 
-\i '/home/ivan/RiderProjects/WebApplication1/res/sql/constraints.sql';
+\i '/home/ivan/RiderProjects/samples/WebApplication1/res/sql/constraints.sql';
 commit;
 
